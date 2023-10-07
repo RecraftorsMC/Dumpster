e89123f298bb79afae61bf204d3d80c7b61b0240
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-10-08 00:40:51 +0200

    Patch gradle screaming at some unknown unchecked operations

--- src/main/java/mc/recraftors/dumpster/client/ClientLiteralArgumentBuilder.java
+++ src/main/java/mc/recraftors/dumpster/client/ClientLiteralArgumentBuilder.java
@@ -1,10 +1,13 @@
 package mc.recraftors.dumpster.client;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.network.ClientCommandSource;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+@Environment(EnvType.CLIENT)
 public final class ClientLiteralArgumentBuilder extends LiteralArgumentBuilder<ClientCommandSource> {
     public ClientLiteralArgumentBuilder(String literal) {
         super(literal);
